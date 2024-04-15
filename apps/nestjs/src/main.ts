8a9d9f96d92cc2b893122608202db8c55b5bbe0a
s1gr1d <sigrid.huemer@posteo.at>
2024-04-15 14:23:22 +0200

    add nestjs application

--- apps/nestjs/src/main.ts
+++ apps/nestjs/src/main.ts
@@ -1,26 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import * as Sentry from '@sentry/node';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './../../.env' });
 
 async function bootstrap() {
   Sentry.init({
     environment: 'qa', // dynamic sampling bias to keep transactions
-    dsn: process.env.SENTRY_DSN,
-    includeLocalVariables: true,
+    dsn: process.env.E2E_TEST_DSN,
     tunnel: `http://localhost:3031/`, // proxy server
     tracesSampleRate: 1,
   });
 
   const app = await NestFactory.create(AppModule);
-
-  app.use(Sentry.Handlers.requestHandler());
-  app.use(Sentry.Handlers.tracingHandler());
-
-  // Uncaught exceptions are not sent as event in v7, in v8 they are sent as events and transactions
-  app.use(Sentry.Handlers.errorHandler());
+  Sentry.setupNestErrorHandler(app);
 
   await app.listen(3030);
 }
