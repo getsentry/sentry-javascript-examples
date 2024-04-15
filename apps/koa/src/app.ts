688418fa9257a346c5eea6bc68ac76695bebff8a
s1gr1d <sigrid.huemer@posteo.at>
2024-04-15 13:37:21 +0200

    add koa application

--- apps/koa/src/app.ts
+++ apps/koa/src/app.ts
@@ -1,10 +1,17 @@
+import * as Sentry from '@sentry/node';
+
+Sentry.init({
+  environment: 'qa', // dynamic sampling bias to keep transactions
+  dsn: process.env.E2E_TEST_DSN,
+  includeLocalVariables: true,
+  debug: true,
+  tunnel: `http://localhost:3031/`, // proxy server
+  tracesSampleRate: 1,
+});
+
 import Koa from 'koa';
 import Router from '@koa/router';
-import * as Sentry from '@sentry/node';
 import { stripUrlQueryAndFragment } from '@sentry/utils';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './../../.env' });
 
 declare global {
   namespace globalThis {
@@ -14,16 +21,6 @@ declare global {
 
 const router = new Router();
 const app = new Koa();
-
-Sentry.init({
-  environment: 'qa', // dynamic sampling bias to keep transactions
-  dsn: process.env.SENTRY_DSN,
-  includeLocalVariables: true,
-  debug: true,
-  tunnel: `http://localhost:3031/`, // proxy server
-  tracesSampleRate: 1,
-  integrations: [...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()],
-});
 
 router.get('/test-success', ctx => {
   ctx.body = { version: 'v1' };
@@ -93,66 +90,14 @@ router.get('/test-local-variables-caught', ctx => {
   ctx.body = { exceptionId, randomVariableToRecord };
 });
 
-const requestHandler = (ctx: { request: Sentry.PolymorphicRequest }, next: () => void) => {
-  Sentry.runWithAsyncContext(() => {
-    const scope = Sentry.getCurrentScope();
-    scope.addEventProcessor(event =>
-      Sentry.addRequestDataToEvent(event, ctx.request, {
-        include: {
-          user: false,
-        },
-      }),
-    );
-
-    next();
-  });
-};
-
-// this tracing middleware creates a transaction per request
-const tracingMiddleWare = (ctx: any, next: () => void) => {
-  const reqMethod = (ctx.method || '').toUpperCase();
-  const reqUrl = ctx.url && stripUrlQueryAndFragment(ctx.url);
-
-  // connect to trace of upstream app
-  let traceparentData;
-  if (ctx.request.get('sentry-trace')) {
-    traceparentData = Sentry.extractTraceparentData(ctx.request.get('sentry-trace'));
-  }
-
-  const transaction = Sentry.startTransaction({
-    name: `${reqMethod} ${reqUrl}`,
-    op: 'http.server',
-    ...traceparentData,
-  });
-
-  ctx.__sentry_transaction = transaction;
-
-  // We put the transaction on the scope so users can attach children to it
-  Sentry.getCurrentScope().setSpan(transaction);
-
-  ctx.res.on('finish', () => {
-    // Push `transaction.finish` to the next event loop so open spans have a chance to finish before the transaction closes
-    setImmediate(() => {
-      // if using koa router, a nicer way to capture transaction using the matched route
-      if (ctx._matchedRoute) {
-        const mountPath = ctx.mountPath || '';
-        transaction.setName(`${reqMethod} ${mountPath}${ctx._matchedRoute}`);
-      }
-      transaction.setHttpStatus(ctx.status);
-      transaction.finish();
-    });
-  });
-
-  next();
-};
-
-app.use(requestHandler);
-app.use(tracingMiddleWare);
+Sentry.setupKoaErrorHandler(app);
 
 app.on('error', (err, ctx) => {
-  Sentry.withScope(scope => {
-    scope.setSDKProcessingMetadata({ request: ctx.request });
-    Sentry.captureException(err);
+  console.log('error', err);
+
+  ctx.body({
+    error: err.message,
+    status: ctx.status,
   });
 });
 
