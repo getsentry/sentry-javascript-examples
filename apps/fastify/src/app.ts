c6fc59d8d8c63c40ebfdea80b76724bd4cb0bf35
s1gr1d <sigrid.huemer@posteo.at>
2024-04-16 09:23:53 +0200

    add fastify application

---
+++ apps/fastify/src/app.ts
@@ -0,0 +1,94 @@
+import * as Sentry from '@sentry/node';
+
+Sentry.init({
+  environment: 'qa', // dynamic sampling bias to keep transactions
+  dsn: process.env.E2E_TEST_DSN,
+  includeLocalVariables: true,
+  integrations: [],
+  tracesSampleRate: 1,
+  tunnel: 'http://localhost:3031/', // proxy server
+});
+
+import { fastify } from 'fastify';
+
+declare global {
+  namespace globalThis {
+    var transactionIds: string[];
+  }
+}
+
+// Make sure fastify is imported after Sentry is initialized
+const app = fastify();
+
+// @ts-ignore
+Sentry.setupFastifyErrorHandler(app);
+
+app.get('/test-success', function (_req, res) {
+  res.send({ version: 'v1' });
+});
+
+app.get('/test-error', async function (req, res) {
+  const exceptionId = Sentry.captureException(new Error('This is an error'));
+
+  await Sentry.flush(2000);
+
+  res.send({ exceptionId });
+});
+
+app.get<{ Params: { param: string } }>('/test-param-success/:param', function (req, res) {
+  res.send({ paramWas: req.params.param });
+});
+
+app.get<{ Params: { param: string } }>('/test-param-error/:param', async function (req, res) {
+  const exceptionId = Sentry.captureException(new Error('This is an error'));
+
+  await Sentry.flush(2000);
+
+  res.send({ exceptionId, paramWas: req.params.param });
+});
+
+app.get('/test-success-manual', async function (req, res) {
+  Sentry.startSpan({ name: 'test-span' }, () => {
+    Sentry.startSpan({ name: 'child-span' }, () => {});
+  });
+
+  await Sentry.flush();
+
+  res.send({
+    transactionIds: global.transactionIds || [],
+  });
+});
+
+app.get('/test-error-manual', async function (req, res) {
+  Sentry.startSpan({ name: 'test-span' }, () => {
+    Sentry.startSpan({ name: 'child-span' }, () => {
+      Sentry.captureException(new Error('This is an error'));
+    });
+  });
+
+  await Sentry.flush(2000);
+
+  res.send({
+    transactionIds: global.transactionIds || [],
+  });
+});
+
+app.get('/test-local-variables-uncaught', function (req, res) {
+  const randomVariableToRecord = 'LOCAL_VARIABLE';
+  throw new Error(`Uncaught Local Variable Error - ${JSON.stringify({ randomVariableToRecord })}`);
+});
+
+app.get('/test-local-variables-caught', function (req, res) {
+  const randomVariableToRecord = 'LOCAL_VARIABLE';
+
+  let exceptionId: string;
+  try {
+    throw new Error('Local Variable Error');
+  } catch (e) {
+    exceptionId = Sentry.captureException(e);
+  }
+
+  res.send({ exceptionId, randomVariableToRecord });
+});
+
+app.listen({ port: 3030 });
