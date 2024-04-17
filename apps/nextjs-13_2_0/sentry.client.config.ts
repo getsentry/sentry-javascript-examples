776a0936b3290696208567c71835e0832731ea63
s1gr1d <sigrid.huemer@posteo.at>
2024-04-17 13:39:36 +0200

    fix next 13.2

--- apps/nextjs-13_2_0/sentry.client.config.ts
+++ apps/nextjs-13_2_0/sentry.client.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   environment: 'qa', // dynamic sampling bias to keep transactions
-  dsn: 'https://3b6c388182fb435097f41d181be2b2ba@o4504321058471936.ingest.sentry.io/4504321066008576',
+  dsn: process.env.E2E_TEST_DSN,
   includeLocalVariables: true,
   tunnel: `http://localhost:3031/`, // proxy server
   tracesSampleRate: 1,
