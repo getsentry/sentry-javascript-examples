385fe0823992f3499eae48da5a481a836d5239e6
s1gr1d <sigrid.huemer@posteo.at>
2024-04-22 13:30:46 +0200

    update nextjs 13.2.0 to Sentry v8

--- apps/nextjs-13_2_0/sentry.edge.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  environment: 'qa', // dynamic sampling bias to keep transactions
-  dsn: process.env.SENTRY_DSN,
-  includeLocalVariables: true,
-  tunnel: `http://localhost:3031/`, // proxy server
-  tracesSampleRate: 1,
-});
