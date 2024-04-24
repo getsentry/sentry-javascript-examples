f3ada63b5c41e99f5ab551824b074605053a369e
s1gr1d <sigrid.huemer@posteo.at>
2024-04-24 10:54:45 +0200

    add crons v7

---
+++ apps/express_profiling-crons/src/app.ts
@@ -0,0 +1,69 @@
+import * as Sentry from '@sentry/node';
+import express from 'express';
+import dotenv from 'dotenv';
+import { CronJob } from 'cron';
+import cron from 'node-cron';
+import * as schedule from 'node-schedule';
+
+dotenv.config({ path: './../../.env' });
+
+const app = express();
+const port = 3030;
+
+Sentry.init({
+  environment: 'qa', // dynamic sampling bias to keep transactions
+  dsn: process.env.SENTRY_DSN,
+  includeLocalVariables: true,
+  debug: true,
+  tunnel: `http://localhost:3031/`, // proxy server
+  tracesSampleRate: 1,
+  profilesSampleRate: 1,
+  integrations: [new Sentry.Integrations.Express({ app })],
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+// cron
+const CronJobWithCheckIn = Sentry.cron.instrumentCron(CronJob, 'cron_slug');
+
+const job = CronJobWithCheckIn.from({
+  cronTime: '7 * * * * *',
+  onTick: () => {
+    console.log('cron: Job is running every few seconds');
+  },
+});
+
+job.start();
+
+// node-cron
+const cronWithCheckIn = Sentry.cron.instrumentNodeCron(cron);
+
+cronWithCheckIn.schedule(
+  '11 * * * * *',
+  () => {
+    console.log('node-cron: Job is running every few seconds');
+  },
+  { name: 'node-cron_slug' },
+);
+
+// node-schedule
+const scheduleWithCheckIn = Sentry.cron.instrumentNodeSchedule(schedule);
+
+scheduleWithCheckIn.scheduleJob('node-schedule_slug', '13 * * * * *', () => {
+  console.log('node-schedule: Job is running every few seconds');
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
+// @ts-ignore
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
