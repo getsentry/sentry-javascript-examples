f89bb408dffd8c27d06d9869d26a23e4e63df621
s1gr1d <sigrid.huemer@posteo.at>
2024-04-25 10:56:55 +0200

    add pages-rotuer next 14 (v7)

--- apps/nextjs-14_2_1/sentry.client.config.ts
+++ apps/nextjs-14_2_1/sentry.client.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   environment: 'qa', // dynamic sampling bias to keep transactions
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   includeLocalVariables: true,
   tunnel: `http://localhost:3031/`, // proxy server
   tracesSampleRate: 1,
