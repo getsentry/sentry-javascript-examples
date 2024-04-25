23e1e178b20337ee7084d8127af60c1a2b021cb6
s1gr1d <sigrid.huemer@posteo.at>
2024-04-25 14:11:46 +0200

    pages-router prod build v7

--- apps/nextjs-13_2_0/app/api/test-local-variables-uncaught/route.ts
+++ apps/nextjs-13_2_0/app/api/test-local-variables-uncaught/route.ts
@@ -1,4 +1,6 @@
-export async function GET() {
+export const dynamic = 'force-dynamic';
+
+async function GET() {
   const randomVariableToRecord = 'LOCAL_VARIABLE';
   throw new Error(`Uncaught Local Variable Error - ${JSON.stringify({ randomVariableToRecord })}`);
 }
