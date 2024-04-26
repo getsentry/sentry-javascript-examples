06440f73de8ac9087bfa51c1c806256a30deeb68
s1gr1d <sigrid.huemer@posteo.at>
2024-04-26 14:43:18 +0200

    update to beta.4

--- apps/nextjs-13_2_0/app/api/test-local-variables-uncaught/route.ts
+++ apps/nextjs-13_2_0/app/api/test-local-variables-uncaught/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const randomVariableToRecord = 'LOCAL_VARIABLE';
   throw new Error(`Uncaught Local Variable Error - ${JSON.stringify({ randomVariableToRecord })}`);
