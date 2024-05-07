cf69fde2716bcf108d932b720f280d5dd10bfc66
s1gr1d <sigrid.huemer@posteo.at>
2024-05-07 12:57:56 +0200

    add external instrumentation file

---
+++ apps/express/src/instrumentation.ts
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/node';
+
+Sentry.init({
+  environment: 'qa', // dynamic sampling bias to keep transactions
+  dsn: process.env.SENTRY_DSN,
+  includeLocalVariables: true,
+  tunnel: `http://localhost:3031/`, // proxy server
+  tracesSampleRate: 1,
+  integrations: integrations =>
+    // todo: When LocalVariablesAsync is included, the debugger initializes every second when running the app with the '--require' flag.
+    integrations.filter(integration => integration.name !== 'LocalVariablesAsync'),
+});
