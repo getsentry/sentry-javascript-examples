50a06cef61598374a0b51ac0e8a8db920234d904
s1gr1d <sigrid.huemer@posteo.at>
2024-05-16 16:55:39 +0200

    update nestjs

--- apps/nestjs/src/main.ts
+++ apps/nestjs/src/main.ts
@@ -1,21 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import './instrument';
 import * as Sentry from '@sentry/node';
+import { BaseExceptionFilter, HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './../../.env' });
 
 async function bootstrap() {
-  Sentry.init({
-    environment: 'qa', // dynamic sampling bias to keep transactions
-    dsn: process.env.SENTRY_DSN,
-    tunnel: `http://localhost:3031/`, // proxy server
-    tracesSampleRate: 1,
-  });
-
   const app = await NestFactory.create(AppModule);
-  // @ts-ignore
-  Sentry.setupNestErrorHandler(app);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter));
 
   await app.listen(3030);
 }
