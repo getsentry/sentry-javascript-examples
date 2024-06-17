7cff7e65154c31988b6e2fcbf2df1b605ccf14b1
s1gr1d <sigrid.huemer@posteo.at>
2024-06-17 13:30:02 +0200

    feat(nextjs): Upgrade sentry to v8

--- apps/nextjs-13_2_0/instrumentation.ts
+++ apps/nextjs-13_2_0/instrumentation.ts
@@ -1,6 +1,9 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs' || process.env.NEXT_RUNTIME === 'edge') {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config');
-    // You can also import a `sentry.edge.config` file if you need to have a different configuration for Edge Functions
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
   }
 }
