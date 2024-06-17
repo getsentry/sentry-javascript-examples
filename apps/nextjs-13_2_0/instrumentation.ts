3fbd584a028b28897d4458b9db2338da7aaaf0a8
s1gr1d <sigrid.huemer@posteo.at>
2024-06-17 12:09:10 +0200

    feat(nextjs): Upgrade sentry to v8

--- apps/nextjs-13_2_0/instrumentation.ts
+++ apps/nextjs-13_2_0/instrumentation.ts
@@ -1,13 +1,6 @@
-import * as Sentry from '@sentry/nextjs';
-
-export function register() {
+export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs' || process.env.NEXT_RUNTIME === 'edge') {
-    Sentry.init({
-      environment: 'qa', // dynamic sampling bias to keep transactions
-      dsn: process.env.E2E_TEST_DSN,
-      includeLocalVariables: true,
-      tunnel: `http://localhost:3031/`, // proxy server
-      tracesSampleRate: 1,
-    });
+    await import('./sentry.server.config');
+    // You can also import a `sentry.edge.config` file if you need to have a different configuration for Edge Functions
   }
 }
