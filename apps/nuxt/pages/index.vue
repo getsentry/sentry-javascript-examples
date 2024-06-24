9428bc7d564c8d804f2d6c3a174e25a830bdce50
s1gr1d <sigrid.huemer@posteo.at>
2024-06-24 15:41:43 +0200

    feat(nuxt): Add nuxt example application

---
+++ apps/nuxt/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><button @click="triggerError">Trigger Error</button><br /></div>
+</template>
+
+<script>
+export default {
+  methods: {
+    triggerError() {
+      throw new Error('This is a test error for Sentry');
+    },
+  },
+};
+</script>
